617c719bf8b0a6cc9faf3468276197fd88eea517
alireza alizadeh <alireza.aa9700@gmail.com>
2022-09-24 12:36:52 +0330

    add third function and the test

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,18 +6,45 @@ def deploye_simple_storage():
     account = get_account()
     # account = accounts.load("alireza-account")
     # account = accounts.add(os.getenv("PRIVATE_KEY"))
-    # account = accounts.add(config["wallets"]["from_key"])
+    # account = accounts.add(config["wallets"]["from_key"]) ---> use in get account function
+
+    # default transactin
+
+    # deploy contract
     simple_storage = SimpleStorage.deploy({"from": account})
+    # with view function we don't have to add from account
     stored_value = simple_storage.retrieve()
-    print(stored_value)
-    transaction = simple_storage.store(15, {"from": account})
-    transaction.wait(1)  # how many block we want to wait
-    # update
-    update_stored_value = simple_storage.retrieve()
-    print(update_stored_value)
+    print("it's the default value: ", stored_value)
+
+    # store function transaction
+
+    # deploy contract
+    storing_num = int(input("enter your number for storing in favorite number: "))
+    transaction = simple_storage.store(storing_num, {"from": account})
+    # how many block we want to wait
+    transaction.wait(1)
+    # update and call the retrieve function again
+    updated_stored_value = simple_storage.retrieve()
+    print("update stored value: ", updated_stored_value)
+
+    # add person transaction
+    print("add person transaction")
+
+    name = input("enter your name: ")
+    number = int(input("enter your number: "))
+
+    add_transaction = simple_storage.addPerson(name, number)
+    add_transaction.wait(1)
+    print(
+        f"your entry value: {simple_storage.people(0)},\n\n tracing by mapping with the name of {name} the number is: {simple_storage.nameToFavoriteNumber(name)}\n\n"
+    )
 
 
 def get_account():
+    """
+    if we're working on a development chain we'll use account zero.
+    and if not we'll use the method that pulls from our config.
+    """
     if (
         network.show_active() == "development"
     ):  # network another key word that brownie actually has
--- tests/test_simple_storage.py
+++ tests/test_simple_storage.py
@@ -2,25 +2,54 @@ from brownie import SimpleStorage, accounts
 
 
 def test_deploy():
+    """
+    in this section we want to get default value and see
+    is it equal to 0 or not?!
+    """
     # arrange
-    account = accounts[0]  # we can actually make contracts
+    account = accounts[0]
+
     # act
-    simple_storage = SimpleStorage.deploy({"from": account})  # deploy
-    starting_value = simple_storage.retrieve()  # call and see start value
+    simple_storage = SimpleStorage.deploy({"from": account})
+
+    # expected to be zero
+    starting_value = simple_storage.retrieve()
     expected = 0
-    # assert
     assert starting_value == expected
 
 
-def test_update_storage():
+def test_updating_storage():
+    """
+    in this section we want to set a value and see
+    is it equal to expected number or not?!
+    """
     # arrange
     account = accounts[0]
-    # in this one arranging is also going to be deploying our smart contract
-    simple_storage = SimpleStorage.deploy({"from": account})
+
     # act
-    expected = 15
-    simple_storage.store(
-        expected, {"from": account}
-    )  # we want to store 15 in our contract
+    simple_storage = SimpleStorage.deploy({"from": account})
+
+    # expected to be the given number
+    expected = 5
+    simple_storage.store(expected, {"from": account})
+
     # assert
-    assert expected == simple_storage.retrieve()
+    assert simple_storage.retrieve() == expected
+
+
+def test_add_person():
+    """
+    in this section we want to set a value and owner of the value
+    and see is the owner name equal to the owner number or not?!
+    """
+    # arrange
+    account = accounts[0]
+
+    # act
+    simple_storage = SimpleStorage.deploy({"from": account})
+
+    expected_number = 15
+    simple_storage.addPerson("alireza", expected_number)
+
+    # assert
+    assert simple_storage.nameToFavoriteNumber("alireza") == expected_number
